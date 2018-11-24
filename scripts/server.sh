1c6f3bf96da59f2e11682b1b007ffe53a1e1bf5b
Sylvain Gournay <gournay.sylvain@gmail.com>
2018-11-24 11:36:54 +0100

    Initial     - creation Jekyll     - import posts

---
+++ scripts/server.sh
@@ -0,0 +1,5 @@
+
+docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data -p 4000:4000 ruby bundle install --path vendor
+
+docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data -p 4000:4000 ruby bundle exec jekyll serve --host 0.0.0.0
+
