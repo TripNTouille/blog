638ba236f70bc8912544c30b0bf201fe39e04711
Sylvain Gournay <gournay.sylvain@gmail.com>
2019-03-02 01:27:19 +0100

    Auto image pour les recettes

---
+++ _plugins/recipe-auto-image.rb
@@ -0,0 +1,8 @@
+Jekyll::Hooks.register :site, :post_read do |site|
+    site.collections['recipes'].docs.each do |recipe|
+        generator = Jekyll::AutoImageGenerator.new
+        generator.instance_variable_set(:@site, site)
+        img = generator.get_image(recipe)
+        recipe.data['image'] = img if img
+    end
+end
