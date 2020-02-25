25de46eba9a7a303d87624947d547fabbc3f8a59
Sylvain Gournay <gournay.sylvain@gmail.com>
2020-02-25 10:21:45 +0100

    Docker scripts specifying container version

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,5 +1,5 @@
 
-docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data ruby bundle install --path vendor
+docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data ruby:2.5 bundle install --path vendor
 
-docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data -p 4000:4000 ruby bundle exec jekyll serve --host 0.0.0.0 $@
+docker run --rm -v $PWD:/data -w /data -e BUNDLE_APP_CONFIG=/data -p 4000:4000 ruby:2.5 bundle exec jekyll serve --host 0.0.0.0 $@
 
